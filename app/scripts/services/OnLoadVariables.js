46ce3b7e2db281d85c5b5ebd1e5ff4cfb7f4da0a
AdamRPC <adamrpcs@gmail.com>
2016-10-06 16:49:05 +0200

    Convert OnLoadVariables.as

---
+++ app/scripts/services/OnLoadVariables.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('cocjs').factory('OnLoadVariables', function ( ) {
+	return {
+		notes : '',
+		date: new Date(),
+		dungeonLoc: 0,
+		inRoomedDungeon: false,
+		inRoomedDungeonResume: null,
+		plotFight: false,
+		timeQ: 0,
+		campQ: false
+	};
+});
