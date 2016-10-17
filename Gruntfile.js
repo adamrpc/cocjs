845a75f3f491167aa5090d4b84d4ca87795ec6ce
AdamRPC <adamrpcs@gmail.com>
2016-10-17 11:42:34 +0200

    Add subfolders for jshint tasks.

--- Gruntfile.js
+++ Gruntfile.js
@@ -130,7 +130,7 @@ module.exports = function (grunt) {
       all: {
         src: [
           'Gruntfile.js',
-          '<%= yeoman.app %>/scripts/{,*/}*.js'
+          '<%= yeoman.app %>/scripts/**/*.js'
         ]
       },
       test: {
