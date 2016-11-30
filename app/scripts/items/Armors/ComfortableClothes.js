20bfdf877292b4bcb54dd9430bf2558196628a14
AdamRPC <adamrpcs@gmail.com>
2016-11-30 13:53:30 +0100

    Fix bad init metod name.

--- app/scripts/items/Armors/ComfortableClothes.js
+++ app/scripts/items/Armors/ComfortableClothes.js
@@ -2,7 +2,7 @@
 
 angular.module( 'cocjs' ).factory( 'ComfortableClothes', function( Armor, CoC ) {
 	var ComfortableClothes = angular.copy( Armor );
-	ComfortableClothes.prototype.ComfortableClothes = function( that ) {
+	ComfortableClothes.prototype.init = function( that ) {
 		Armor.prototype.init( that, ['C.Cloth', 'C.Cloth', 'comfortable clothes', 'a set of comfortable clothes', 0, 0, 'These loose fitting and comfortable clothes allow you to move freely while protecting you from the elements.  (DEF) (Cost: 0)', 'Light', true] );
 	};
 	ComfortableClothes.prototype.supportsBulge = function() {
