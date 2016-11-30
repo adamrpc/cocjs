5ecd60ae0daf3cee38adaf689e3092b368779529
AdamRPC <adamrpcs@gmail.com>
2016-11-30 16:25:58 +0100

    Convert BeautifulSword.as

---
+++ app/scripts/items/Weapons/BeautifulSword.js
@@ -0,0 +1,30 @@
+'use strict';
+
+angular.module( 'cocjs' ).factory( 'BeautifulSword', function( CoC, Utils, Weapon, EngineCore ) {
+	var BeautifulSword = angular.copy( Weapon );
+	BeautifulSword.prototype.init = function( that ) {
+		Weapon.prototype.init( that, [ 'B.Sword', 'B.Sword', 'beautiful sword', 'a beautiful shining sword', 'slash', 7, 400, 'This beautiful sword shines brilliantly in the light, showing the flawless craftsmanship of its blade.  The pommel and guard are heavily decorated in gold and brass.  Some craftsman clearly poured his heart and soul into this blade.  (ATK) (Cost: 400)', 'holySword' ] );
+	};
+	BeautifulSword.prototype.canUse = function() {
+		if( CoC.getInstance().player.cor < 35 ) {
+			return true;
+		}
+		EngineCore.outputText( 'You grab hold of the handle of the sword only to have it grow burning hot.  You\'re forced to let it go lest you burn yourself.  Something within the sword must be displeased.  ' );
+		return false;
+	};
+	return new Proxy( BeautifulSword, {
+		construct: function( target ) {
+			return new Proxy( target, {
+				get: function( target, name ) {
+					if( name === 'attack' ) {
+						return 7 + Math.ceil( 10 - CoC.getInstance().player.cor / 3 );
+					}
+					return target[ name ];
+				},
+				set: function( target, name, value ) {
+					target[ name ] = value;
+				}
+			} );
+		}
+	} );
+} );
