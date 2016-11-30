3ef5afedda8259569e69d10184fce8d223d77896
AdamRPC <adamrpcs@gmail.com>
2016-11-30 13:50:04 +0100

    Convert ArmorWithPerk.as

---
+++ app/scripts/items/Armors/ArmorWithPerk.js
@@ -0,0 +1,29 @@
+'use strict';
+
+angular.module( 'cocjs' ).factory( 'Armor', function( Armor, CoC ) {
+	var ArmorWithPerk = angular.copy( Armor );
+	ArmorWithPerk.prototype.init = function( that, args ) {
+		Armor.prototype.init( that, [ args[ 0 ], args[ 1 ], args[ 2 ], args[ 3 ], args[ 4 ], args[ 5 ], args[ 6 ], args[ 7 ], args.length > 14 ? args[ 14 ] : false ] );
+		that.playerPerk = args[ 8 ];
+		that.playerPerkV1 = args[ 9 ];
+		that.playerPerkV2 = args[ 10 ];
+		that.playerPerkV3 = args[ 11 ];
+		that.playerPerkV4 = args[ 12 ];
+	};
+	ArmorWithPerk.prototype._superPlayerEquip = ArmorWithPerk.prototype.playerEquip;
+	ArmorWithPerk.prototype.playerEquip = function() { //This item is being equipped by the player. Add any perks, etc.
+		while( CoC.getInstance().player.findPerk( this.playerPerk ) >= 0 ) {
+			CoC.getInstance().player.removePerk( this.playerPerk );
+		}
+		CoC.getInstance().player.createPerk( this.playerPerk, this.playerPerkV1, this.playerPerkV2, this.playerPerkV3, this.playerPerkV4 );
+		return this._superPlayerEquip();
+	};
+	ArmorWithPerk.prototype._superPlayerRemove = ArmorWithPerk.prototype.playerRemove;
+	ArmorWithPerk.prototype.playerRemove = function() { //This item is being removed by the player. Remove any perks, etc.
+		while( CoC.getInstance().player.findPerk( this.playerPerk ) >= 0 ) {
+			CoC.getInstance().player.removePerk( this.playerPerk );
+		}
+		return this._superPlayerRemove();
+	};
+	return ArmorWithPerk;
+} );
